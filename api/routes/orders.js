9440c26b94404f63c68eddf5fb6ea8173b33513a
ErnieBuyco <buycoernie@gmail.com>
2019-09-09 21:54:17 +0800

    date--install

---
+++ api/routes/orders.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Orders where fetched',
+  });
+});
+
+router.post('/', (req, res, next) => {
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity
+  };
+  res.status(201).json({
+    message: 'Order post data from /orders, Order just created',
+    /*eslint-disable*/
+    order: order
+  });
+});
+
+
+router.get(':/orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Order Details',
+    orderId: req.params.orderId,
+  });
+});
+
+router.delete(':/orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Order deleted',
+    orderId: req.params.orderId,
+  });
+});
+
+module.exports = router;
